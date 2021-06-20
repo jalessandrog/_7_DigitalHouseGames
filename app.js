820e72def9478bc19f1914c25b21a4a204b9778f
jalessandrog <jalessandro.ag@gmail.com>
2021-06-19 23:05:55 -0500

    Estructura utilizando Node.js+Express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+app.use('/static', express.static('public'));
+
+
+app.listen(3000, () =>{
+    console.log('Servidor funcionando en el puerto 3000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+})
