fb5af90f3fa3554b82f7fa16cf49f585c5ce26c3
Carlos Zepeda <carlosjize@gmail.com>
2021-08-08 14:55:03 -0500

    Pagina de nintendo, xbox y ps, varios filtros

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const listpro = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-//const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 const controller = {
     index: (req, res) => {
@@ -13,7 +13,7 @@ const controller = {
     detail: (req, res) => {
         let id= parseInt(req.params.id,10)
         const complist = listpro.find(p => p.id === id)
-        res.render('detail', {title: 'Detalles del producto', cssFile : 'style',listpro2:complist})
+        res.render('detail', {title: 'Detalles del producto', cssFile : 'style',listpro2:complist,listpro:listpro, toThousand:toThousand })
     },
     basket: (req, res) =>{
          res.render('shopping-cart',{ title: 'Carrito de Compras', cssFile: 'styles_productCar'})
@@ -47,8 +47,17 @@ const controller = {
         res.redirect('/products/all')
     },
     todos:(req, res) => {
-        res.render('all', {title: 'Detalles del producto', cssFile : 'style', listpro:listpro})
-    } 
+        res.render('all', {title: 'Detalles del producto', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+    },
+    ni:(req, res) => {
+        res.render('Nintendo', {title: 'Detalles del producto', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+    },
+    ps:(req, res) => {
+        res.render('PS', {title: 'Detalles del producto', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+    },
+    xb:(req, res) => {
+        res.render('Xbox', {title: 'Detalles del producto', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+    }    
 };
 
 module.exports = controller;
--- routes/products.js
+++ routes/products.js
@@ -17,7 +17,9 @@ router.get('/basket', productsController.basket);
 router.get('/addproduct', productsController.create);
 router.post('/addproduct',upload.single('imagenPrincipal'),productsController.guardar)
 router.get('/all',productsController.todos)
-
+router.get('/Nintendo',productsController.ni)
+router.get('/PS',productsController.ps)
+router.get('/Xbox',productsController.xb)
 
 
 module.exports = router;
