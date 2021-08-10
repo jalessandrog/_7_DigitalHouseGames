75e39e6d2740bdb572f0903aa606e0d94ef8e38e
jalessandrog <jalessandro.ag@gmail.com>
2021-08-09 22:39:54 -0500

    Funcionalidad agregada :  guardar datos de usuario registrado en archivo JSON

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require("bcryptjs");
 
+
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.JSON');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const listpro = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -14,7 +17,34 @@ const controller = {
 	},
 	signup: (req, res)=>{
 		res.render('register', {title: 'Crear Cuenta', cssFile : 'style'})  
-	}
+	},
+	saveUser: (req, res)=>{
+		let passEncriptada = bcrypt.hashSync(req.body.password, 10);
+
+		let usuario= {
+            id: Date.now(),
+            nombre:req.body.nombre,
+			apellidos:req.body.apellidos,
+			email:req.body.email,
+			password:passEncriptada,
+			cumpleanios:req.body.cumpleanios,
+            tipo_usuario: req.body.tipousuario,
+            avatar:req.file.filename
+        }
+		let usersfile=fs.readFileSync(usersFilePath,{encoding:'utf-8'})
+        let usuarios;
+        if (usersfile==''){
+			usuarios=[]
+        }else{
+			usuarios=JSON.parse(usersfile)
+        }
+        usuarios.push(usuario)
+
+        usuariosJSON= JSON.stringify(usuarios, null, 2)
+
+        fs.writeFileSync(usersFilePath,usuariosJSON)
+        res.redirect('/')
+	},
 };
 
 module.exports = controller;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,16 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const upload=require('../config/multer');
 
 // ************ Controller Require ************
 const usersController = require('../controllers/usersControllers');
 
 router.get('/', usersController.index); 
 router.get('/login', usersController.login);
+
 router.get('/register', usersController.signup);
+router.post('/register',upload.single('avatar'), usersController.saveUser);
 
 
 module.exports = router;
