bb5ac9da9d4b884ac460ee144fef4d2af91fb57b
Carlos Zepeda <carlosjize@gmail.com>
2021-08-23 23:16:14 -0500

    Integrando la base

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -3,6 +3,7 @@ const path = require('path');
 const methodOverride=require('method-override');
 const { body } = require('express-validator');
 let db=require('../database/models');
+const { debugPort } = require('process');
 
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
@@ -12,70 +13,125 @@ const string= n=>n.toString()
 
 const controller = {
     index: (req, res) => {
-        res.render('all', {title: 'Detalles del producto', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+        db.Producto.findAll()
+            .then(function(productos){
+                res.render('all', {title: 'Detalles del producto', cssFile : 'style', listpro:productos, toThousand:toThousand})
+            })
+        // res.render('all', {title: 'Detalles del producto', cssFile : 'style', listpro:listpro, toThousand:toThousand})
     },
     detail: (req, res) => {
-        let id= parseInt(req.params.id,10)
-        const complist = listpro.find(p => p.id === id)
-        res.render('detail', {title: 'Detalles del producto', cssFile : 'style',listpro2:complist,listpro:listpro, toThousand:toThousand })
+        let allproducts=db.Producto.findAll()
+        let productspecific=db.Producto.findByPk(parseInt(req.params.id,10))
+        Promise.all([allproducts,productspecific])
+            .then(function([resultall,prod]){
+                res.render('detail', {title: 'Detalles del producto', cssFile : 'style',listpro2:prod,listpro:resultall, toThousand:toThousand })
+            })
+
+            
+//        let id= parseInt(req.params.id,10)
+//        const complist = listpro.find(p => p.id === id)
+//        res.render('detail', {title: 'Detalles del producto', cssFile : 'style',listpro2:complist,listpro:listpro, toThousand:toThousand })
     },
     basket: (req, res) =>{
          res.render('shopping-cart',{ title: 'Carrito de Compras', cssFile: 'styles_productCar'})
     },
     create: (req, res) => {
-        res.render('product-create-form',{ title: 'Añadir producto', cssFile: 'style'})
+         res.render('product-create-form',{ title: 'Añadir producto', cssFile: 'style'})
     },
     guardar: (req,res)=>{
+
         if(req.file){
             if(req.file.filename){
-                let product= {
-                    id: Date.now(),
+                db.Producto.create({
                     nombre:req.body.nombre,
                     rating: parseInt(req.body.rating,10),
                     precio: parseInt(req.body.precio,10),
-                    plataforma:req.body.plataforma,
-                    consola:req.body.consola,
-                    categoria:req.body.categoria,
                     breveDescripcion: req.body.breveDescripcion,
                     informacionAdicional: req.body.informacionAdicional,
-                    imagenPrincipal:req.file.filename
-                }
-                let archivoproductos=fs.readFileSync(productsFilePath,{encoding:'utf-8'})
-        let productos;
-        if (archivoproductos==''){
-             productos=[]
-        }else{
-             productos=JSON.parse(archivoproductos)
-        }
-        productos.push(product)
-
-        productosJSON= JSON.stringify(productos, null, 2)
-
-        fs.writeFileSync(productsFilePath,productosJSON)
-        res.redirect('/products/all')
+                    imagenPrincipal:req.file.filename,
+                    idPlataforma:parseInt(req.body.plataforma,10),
+                    idConsola:parseInt(req.body.consola,10),
+                    idCategoria:parseInt(req.body.categoria,10)
+                })
+                .then(function(a)
+                {res.redirect('/products/all')})
             }
         }else{
             res.send('Falta adjuntar imagen, intentalo de nuevo')
         }
+//        if(req.file){
+//            if(req.file.filename){
+//                let product= {
+//                    id: Date.now(),
+//                    nombre:req.body.nombre,
+//                    rating: parseInt(req.body.rating,10),
+//                    precio: parseInt(req.body.precio,10),
+//                    plataforma:req.body.plataforma,
+//                    consola:req.body.consola,
+//                    categoria:req.body.categoria,
+//                    breveDescripcion: req.body.breveDescripcion,
+//                    informacionAdicional: req.body.informacionAdicional,
+//                    imagenPrincipal:req.file.filename
+//                }
+//                let archivoproductos=fs.readFileSync(productsFilePath,{encoding:'utf-8'})
+//        let productos;
+//        if (archivoproductos==''){
+//             productos=[]
+//        }else{
+//             productos=JSON.parse(archivoproductos)
+//        }
+//        productos.push(product)
+//
+//        productosJSON= JSON.stringify(productos, null, 2)
+//
+//        fs.writeFileSync(productsFilePath,productosJSON)
+//        res.redirect('/products/all')
+//            }
+//        }else{
+//            res.send('Falta adjuntar imagen, intentalo de nuevo')
+//        }
         
         
     },
     todos:(req, res) => {
-        res.render('all', {title: 'Todos los productos', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+        db.Producto.findAll()
+            .then(function(productos){
+                res.render('all', {title: 'Todos los productos', cssFile : 'style', listpro:productos, toThousand:toThousand})
+            })
+        //res.render('all', {title: 'Todos los productos', cssFile : 'style', listpro:listpro, toThousand:toThousand})
     },
     ni:(req, res) => {
-        res.render('Nintendo', {title: 'Nintendo', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+        db.Producto.findAll()
+            .then(function(productos){
+                res.render('Nintendo', {title: 'Nintendo', cssFile : 'style', listpro:productos, toThousand:toThousand})
+            })
+        // res.render('Nintendo', {title: 'Nintendo', cssFile : 'style', listpro:listpro, toThousand:toThousand})
     },
     ps:(req, res) => {
-        res.render('PS', {title: 'Playstation', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+        db.Producto.findAll()
+            .then(function(productos){
+                res.render('PS', {title: 'Playstation', cssFile : 'style', listpro:productos, toThousand:toThousand})
+            })
+       // res.render('PS', {title: 'Playstation', cssFile : 'style', listpro:listpro, toThousand:toThousand})
     },
     xb:(req, res) => {
-        res.render('Xbox', {title: 'Xbox', cssFile : 'style', listpro:listpro, toThousand:toThousand})
+        db.Producto.findAll()
+            .then(function(productos){
+                res.render('Xbox', {title: 'Xbox', cssFile : 'style', listpro:productos, toThousand:toThousand})
+            })
+        //res.render('Xbox', {title: 'Xbox', cssFile : 'style', listpro:listpro, toThousand:toThousand})
     },
     edit: (req, res) => {
-        let id= parseInt(req.params.id,10)
-        const complist = listpro.find(p => p.id === id)
-        res.render('edit-form',{ title: 'Editar producto', cssFile: 'style',listpro3:complist, string:string})
+        db.Producto.findByPk(parseInt(req.params.id,10),{
+            include:[{association:'consola'}]
+        })
+        .then(function(resprod){
+            res.render('edit-form',{ title: 'Editar producto', cssFile: 'style',listpro3:resprod, string:string})
+        })
+
+//        let id= parseInt(req.params.id,10)
+//        const complist = listpro.find(p => p.id === id)
+//        res.render('edit-form',{ title: 'Editar producto', cssFile: 'style',listpro3:complist, string:string})
     },
     actualizar: (req, res) => {
 
